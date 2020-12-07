d065f3dfc17858880cc146fc5d8613285859074d
Aseem Pandita <aseempandita@live.com>
2020-12-07 20:21:43 +0530

    Create config.py
    Add config file

---
+++ config.py
@@ -0,0 +1,5 @@
+subscription_key = <Add Azure subscription key>
+endpoint = <Add Azure endpoint URL>
+analyze_url = endpoint + "vision/v3.1/analyze"
+ocr_url = endpoint + "vision/v3.1/ocr"
+face_api_url = endpoint + "/face/v1.0/detect"
